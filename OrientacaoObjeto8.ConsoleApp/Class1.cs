1b22874856c3dd713b3572ab353ef31c847d2e60
Vogeel <vitorvogel12@gmail.com>
2022-03-08 16:22:59 -0300

    Add project files.

---
+++ OrientacaoObjeto8.ConsoleApp/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjeto8.ConsoleApp
+{
+    internal class Oleo
+    {
+        public double raio;
+        public double altura;
+        public double volume()
+        {
+            double ValorFinal = 3.14 * raio * raio * altura;
+            return ValorFinal;
+        }
+    }
+}
---
+++ OrientacaoObjeto8.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OrientacaoObjeto8.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Oleo volumeoleo = new Oleo();
+            volumeoleo.raio = 3.5;
+            volumeoleo.altura = 8;
+            Console.WriteLine($"A sua lata de oleo tem {volumeoleo.volume():F2} metros cubicos");
+            Console.ReadKey();
+        }
+    }
+}
